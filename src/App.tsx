24ff649006bc42c8019df29b33ea1a4fd3b9dbe1
jamardines <ardines.main.gg@gmail.com>
2026-04-02 10:52:56 +0800

    feat: AI Chat UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./screens/authscreen/LoginPage";
 import SignUpPage from "./screens/authscreen/SignUpPage";
 import LandingPage from "./screens/LandingPage";
+import AIChat from "./screens/pages/AIChat";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/landing" replace />} />
+        <Route path="/landing" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/chat" element={<AIChat />} />
       </Routes>
     </BrowserRouter>
   );
